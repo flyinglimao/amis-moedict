fcf333048d0e5d38718e9a58eb828bcb85ae8db9
Yu Chen <flyinglimao@aol.com>
2020-10-26 16:06:20 +0000

    add cache

--- sw.js
+++ sw.js
@@ -1,13 +1,5 @@
 const CACHE_NAME = 'cache-v1';
-self.addEventListener('install', function() {
-    console.log('install')
-    caches.open(CACHE_NAME).add('/');
-});
-this.addEventListener('activate', function(event) {
-    console.log('activated!');
-  });
 self.addEventListener('fetch', function(event) {
-    console.log(event.request);
     if (event.request.method != 'GET') return;
 
     event.respondWith(async function() {
@@ -15,7 +7,7 @@ self.addEventListener('fetch', function(event) {
         const cachedResponse = await cache.match(event.request);
 
         if (cachedResponse) {
-            event.waitUntil(cache.add(event.request)).cache(_ => {});
+            event.waitUntil(cache.add(event.request));
             return cachedResponse;
         }
 
